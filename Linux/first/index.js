d9695a75fd2a7067097907025cb4672c3a315ecc
VISKI NATALINA <overlordnatalina@gmail.com>
2019-11-08 17:02:28 +0200

    f

--- Linux/first/index.js
+++ Linux/first/index.js
@@ -5,7 +5,10 @@ app.use (express.static('public'))
 app.get('/',  (req, res)=> {
   res.sendFile(`${__dirname}/public/index.html`)
 })
- 
+app.get('/api',  (req, res)=> {
+  res.sendFile(`${__dirname}/public/indexApi.html`)
+}) 
+
 app.listen(3000, '0.0.0.0', ()=>{
 console.log (`Server Express started in https://0.0.0.0:3000`)
 })
